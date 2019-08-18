8c00ef51168ac30d0790a18f09d83729849586de
lcontreras21 <luiscontrerasorendain@gmail.com>
2019-08-18 08:36:51 -0700

    finished scanning sms and mms data into people class, more logic to follow

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 '''
 I will take in all the text messages me and Helena have sent. I will compile statistics on them and create useful graphs on them.
-Additionally, I can use the language model I made for 208 to create an LM for me or for her or for both and create something that makes dialogue. 
+Additionally, I can use the language model I made for 208 to create an LM for me or for her or for both and create something that makes dialogue.
 
 Outline:
     Read in the xml file containing text.
@@ -12,77 +12,128 @@ Not being built to handle emojies, future implementation?
 
 from xml.dom.minidom import parse
 import xml.dom.minidom
+import time
+import re
+
+
+# TODO Figure out how emojis are sent and keep that as a count too, maybe a dict of emoji types and their counts
+
+
 
 #This function will handle the reading in and stripping of xml stuff
-def pre_process():
+def load_sms_mms_data():
    # Open XML doc using minidom parser
-   DOMTree = xml.dom.minidom.parse("sms_data.xml")
+   DOMTree = xml.dom.minidom.parse("xml_data/sms_data_processed.xml")
    collection = DOMTree.documentElement
 
    #Get all the sms in the collection
    sms = collection.getElementsByTagName("sms")
-   return sms
+   mms = collection.getElementsByTagName("mms")
+   return sms, mms
 
 # This function will create a person class and associate each of the text type to a person
-class person:
-    def __init__(self, type_no):
-        self.type_no = type_no
-        self.sms_list = [] 
+class Person:
+	def __init__(self, type_no):
+		self.type_no = type_no
+		self.sms_list = []
+		self.mms_list = []
 
-    def add_sms(self, sms):
-        self.sms_list.append(sms)
+	def add_sms(self, sms):
+		self.sms_list.append(sms)
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.type_no == other.type_no
-    
-    def __lt__(self, other):
-        return self.type_no < other.type_no
+	def add_mms(self, mms):
+		self.mms_list.append(mms)
 
-    def __str__(self):
-        return "Person "+ str(self.type_no)
+	def __eq__(self, other):
+		if isinstance(other, self.__class__):
+		    return self.type_no == other.type_no and len(self.sms_list) == len(other.sms_list)
 
-class msg:
+	def __lt__(self, other):
+		return self.type_no < other.type_no
+	
+	def scan_for_emojis(self):
+		self.emojis = {}
+		for sms in self.sms_list:
+			matches = re.findall(r"0x[a-z;0-9]{5}", sms.text)
+			if len(matches) != 0:
+				for match in matches:
+					if match not in self.emojis:
+						self.emojis[match] = 1
+					else:
+						self.emojis[match] += 1
+
+	# TODO Make the person iterable based on sms using itertools or something like that, i remember reading about it.
+
+	def __str__(self):
+	    return self.type_nos
+
+class Message:
     def __init__(self, text, date, readable_date):
         #date in milliseconds Unix time
         self.text = text
         self.date = date
         self.readable_date = readable_date
-    
+
     def __str__(self):
         return str(self.text) + " " + str(self.readable_date)
 
-def create_people(sms_data):
-    # create the people found in the text messages
-    people = []
-    types = []
-    for msg in sms_data:
-        if msg.getAttribute("type") not in types:
-            types.append(msg.getAttribute("type"))
-    for type_x in types:
-        new_person = person(type_x)
-        people.append(new_person)
-    return people
+class Photo:
+	def __init__(self, text, date, readable_date):
+		self.text = text
+		self.date = date
+		self.readable_date = readable_date
+		self.contains_image = False
+		self.photo_count = 0
 
-def add_messages(people, sms_data):
-    # go through each sms and add the text to the appropriate person
-    people = people.sort()
-    
-def process():
-    print("Loading XML file", flush=True)
-    sms_data = pre_process()
-    people = create_people(sms_data)
-    for person_x in people.sort():
-        print(person_x)
-    return people
-    # now each person has its own messages related to it
-    # in the form of a list of sms class types
-    
-    # TODO create statistics based on those messages
-    # TODO create plots and graphs based on those statistics
-    # TODO create language model based on each person, based on CS 208 lab
-        # dont have the code for that though at the moment, but I can recall it
+# Returns a dictionary of people associated to their type value.
+# "1" is Helena and "2" is Luis
+def associate_people_to_sms():
+	sms_data, mms_data = load_sms_mms_data()
+	people = {}
 
+	for sms in sms_data:
+		person_id = sms.getAttribute("type")
+		if person_id not in people:
+			people[person_id] = Person(person_id)
 
+		current_sms = Message(sms.getAttribute("body"), sms.getAttribute("data"), sms.getAttribute("readable_data"))
+
+		people[person_id].add_sms(current_sms)
+
+	for mms in mms_data:
+		mms_id = mms.getAttribute("msg_box")
+		
+		mms_date = mms.getAttribute("date")
+		mms_readable_data = mms.getAttribute("readable_data")
+		mms_text = ""
+		mms_contains_image = False
+		mms_count = 0
+
+		part = mms.getElementsByTagName("part")
+		for i in part:
+			if i.getAttribute("ct") == "image/jpeg":
+				mms_contains_image = True
+				mms_count += 1
+			if i.getAttribute("ct") == "text/plain":
+				mms_text = i.getAttribute("text")
+		
+		current_mms = Photo(mms_text, mms_date, mms_readable_data)
+		current_mms.contains_image = mms_contains_image
+		current_mms.photo_count = mms_count
+		
+		people[mms_id].add_mms(current_mms)
+	
+	for person in people:
+		people[person].scan_for_emojis()
+	return people
+	
+
+# TODO create statistics based on those messages
+# TODO create plots and graphs based on those statistics
+
+# TODO create language model based on each person, based on CS 208 lab
 if __name__ == "__main__":
-    process()
+	people = associate_people_to_sms()
+	Helena = people["1"]
+	Luis = people["2"]
+
---
+++ process_xml.py
@@ -0,0 +1,48 @@
+### More preprocessing of xml files to handle emoji characters
+import re
+
+def convert_to_hex(string):
+	# example string would be "&#55358;&#56599"
+	string = string.replace("&#", "").replace(";", " ").split()
+	string = list(map(int, string))
+	
+	# Your decimal UTF-16 number (which is in two separate blocks for now) is 55358 56599
+
+	# You subtract 0xd800 from the first block and 0xdc00 from the second to give 0x3e 0x117
+	string[0] = string[0] - int("0xd800", 16)
+	string[1] = string[1] - int("0xdc00", 16)
+	
+	# Converting them to binary, padding them out to 10 bits and concatenating them, it's 0b0000 1111 1001 0001 0111
+	string = ["{:0>10}".format(bin(i)[2:]) for i in string]
+	string = string[0] + string[1] 
+
+	# Then we convert that back to hex, which is 0x0f917
+	string = hex(int(string, 2))
+
+	# Finally, we add 0x10000, giving 0x1f917
+	string = hex(int(string, 16) + int("0x10000", 16))
+
+	# Therefore, our (hex) HTML entity is &#x1f917;. Or, in decimal, &#129303
+	return string
+
+def preprocess_xml(xml_file):
+	with open(xml_file[:-4] + "_processed.xml", "w") as new_file:
+		with open(xml_file, "r") as old_file:
+			for line in old_file:
+				matches = re.findall(r"&#[0-9]*;", line)
+
+				matches = [i for i in matches if len(i) > 5]
+
+				old_string = "".join(matches)
+				if len(matches) != 0:
+					matches = [matches[i] + matches[i+1] for i in range(0, len(matches), 2)]
+					matches = [convert_to_hex(i) for i in matches]
+
+					new_string = "".join(matches)
+					line = line.replace(old_string, new_string)
+
+				new_file.write(line)
+
+if __name__ == "__main__":
+	#convert_to_hex("&#55358;&#56599")
+	preprocess_xml("xml_data/sms_data.xml")
